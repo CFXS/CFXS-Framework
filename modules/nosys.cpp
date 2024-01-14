434bcb9d68da163fd346838622cc5a6e8687e7cf
Rihards Veips <rihardsveips@gmail.com>
2024-01-14 04:45:08 +0200

    change to nano specs, extend nosys.cpp

--- modules/nosys.cpp
+++ modules/nosys.cpp
@@ -40,4 +40,38 @@ __weak int _write(int file, char *ptr, int len) {
 __weak int _read(int file, char *ptr, int len) {
     return -1;
 }
+
+__weak void abort() {
+    while (1 < 2) {
+    }
 }
+
+extern "C" int __aeabi_atexit(void *obj, void (*dtr)(void *), void *dso_h) {
+    (void)obj;
+    (void)dtr;
+    (void)dso_h;
+    return 0;
+}
+
+void *__dso_handle = 0;
+
+/**
+ * This is an error handler that is invoked by the C++ runtime when a pure virtual function is called.
+ * If anywhere in the runtime of your program an object is created with a virtual function pointer not
+ * filled in, and when the corresponding function is called, you will be calling a 'pure virtual function'.
+ * The handler you describe should be defined in the default libraries that come with your development environment.
+ */
+void __cxa_pure_virtual() {
+    while (1 < 2) {
+    }
+}
+}
+
+namespace __gnu_cxx {
+
+    void __verbose_terminate_handler() {
+        while (1 < 2) {
+        }
+    }
+
+} // namespace __gnu_cxx
